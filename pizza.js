cafdc6e1d6f8937be4f03e19a69d1e8632d525ca
sionesmith <smithsione.s91@gmail.com>
2024-08-21 11:16:57 +1200

    update

--- pizza.js
+++ pizza.js
@@ -1,4 +1,9 @@
 console.log ("i love pizza");
 let pizzaOrder = "meat lovers";
-let price = 10;
+let price = 20;
 console.log ("New pizza order: " + pizzaOrder +". The price of the pizza is: $" + price );
+let orderAmount = 12;
+let familySize = "large pizza";
+let regular = "standerd pizza";
+let totalPrice = orderAmount * price;
+console.log (" New pizza order: " + orderAmount + " " + familySize + " " + pizzaOrder + ". Total cost for the order is: $" + totalPrice);
